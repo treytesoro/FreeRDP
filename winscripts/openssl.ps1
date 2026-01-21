546ec13c0fb669d44375c0f30bc4e0e80b2dc5a5
Trey Tesoro <treytesoro@gmail.com>
2026-01-21 09:09:43 -0500

    powershell build scripts update

--- winscripts/openssl.ps1
+++ winscripts/openssl.ps1
@@ -4,9 +4,11 @@ Write-Host "If failing, make sure synology or drobox folder redirection is not i
 $currentlocation = (Get-Location).Path
 write-host $env:libspath\OPENSSL-install
 git clone https://github.com/openssl/openssl.git
-cd openssl
-perl .\Configure VC-WIN64A no-shared no-tests --prefix="$env:libspath\OPENSSL-install"
+Set-Location openssl
+perl .\Configure VC-WIN64A no-tests --prefix="$env:libspath\OPENSSL-install"
 nmake
 nmake install
 
-cd $currentlocation
+# New-Item "$env:libspath\OPENSSL-install\test.txt" -Force
+
+Set-Location $currentlocation
--- winscripts/setup.ps1
+++ winscripts/setup.ps1
@@ -1,80 +1,93 @@
 $currentlocation = (Get-Location).Path
 
-if($currentlocation -notlike "*winscripts*") {
+if ($currentlocation -notlike "*winscripts*") {
     Write-Host "Please run this script from the winscripts folder."
     exit 1
 }
 
-
-
-cmd.exe /c rmdir /s /q openssl 
-cmd.exe /c rmdir /s /q zlib 
-cmd.exe /c rmdir /s /q zlib-build 
-cmd.exe /c rmdir /s /q uriparser 
-cmd.exe /c rmdir /s /q uriparser-build 
-cmd.exe /c rmdir /s /q cjson 
-cmd.exe /c rmdir /s /q cJSON-build 
-cmd.exe /c rmdir /s /q SDL 
-cmd.exe /c rmdir /s /q SDL-build 
-cmd.exe /c rmdir /s /q SDL_ttf 
-cmd.exe /c rmdir /s /q SDL_ttf-build 
-cmd.exe /c rmdir /s /q SDL_image 
-cmd.exe /c rmdir /s /q SDL_image-build 
-cmd.exe /c rmdir /s /q libusb-cmake 
-cmd.exe /c rmdir /s /q libusb-cmake-build 
-
-
 Write-Host "Setting up dependencies in $currentlocation"
 $env:PATH += ";$currentlocation\python;$currentlocation\python\Scripts;${currentlocation}\strawberry-perl\perl\bin;${currentlocation}\strawberry-perl\perl\site\bin;${currentlocation}\strawberry-perl\c\bin"
 
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.14.2/python-3.14.2-embed-amd64.zip" -OutFile "python.zip"
-mkdir python
-cd python
-tar -xf ..\python.zip
+function Remove-TempBuildFiles {
+    cmd.exe /c rmdir /s /q openssl 
+    cmd.exe /c rmdir /s /q zlib 
+    cmd.exe /c rmdir /s /q zlib-build 
+    cmd.exe /c rmdir /s /q uriparser 
+    cmd.exe /c rmdir /s /q uriparser-build 
+    cmd.exe /c rmdir /s /q cjson 
+    cmd.exe /c rmdir /s /q cJSON-build 
+    cmd.exe /c rmdir /s /q SDL 
+    cmd.exe /c rmdir /s /q SDL-build 
+    cmd.exe /c rmdir /s /q SDL_ttf 
+    cmd.exe /c rmdir /s /q SDL_ttf-build 
+    cmd.exe /c rmdir /s /q SDL_image 
+    cmd.exe /c rmdir /s /q SDL_image-build 
+    cmd.exe /c rmdir /s /q libusb-cmake 
+    cmd.exe /c rmdir /s /q libusb-cmake-build 
+}
 
-# python314._pth edit to uncomment import site
-Set-Content -Path ".\python314._pth" -Value @"
+function Set-BuilderDependencies {
+    if (Test-Path("python")) {
+        # Remove-Item is async and breaks our process
+        cmd.exe /c rmdir /s /q python
+    }
+    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.14.2/python-3.14.2-embed-amd64.zip" -OutFile "python.zip"
+    mkdir python
+    Set-Location python
+    tar -xf ..\python.zip
+
+    # python314._pth edit to uncomment import site (just overwrite for similicity)
+    Set-Content -Path ".\python314._pth" -Value @"
 python314.zip
 .
 
 # Uncomment to run site.main() automatically
 import site
 "@
-Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -OutFile "get-pip.py"
-.\python.exe .\get-pip.py
-# install meson and ninja
-.\python.exe -m pip install meson ninja
-cd ..
+    Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -OutFile "get-pip.py"
+    .\python.exe .\get-pip.py
+    # install meson and ninja
+    .\python.exe -m pip install meson ninja
+    Set-Location ..
 
-Invoke-WebRequest -Uri "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_54201_64bit/strawberry-perl-5.42.0.1-64bit-portable.zip" -OutFile "strawberry-perl.zip"
-mkdir strawberry-perl
-cd strawberry-perl
-tar -xf ..\strawberry-perl.zip
-cd ..
-
-# $env:PATH += ";$currentlocation\python;${currentlocation}\strawberry-perl\perl\bin;${currentlocation}\strawberry-perl\perl\site\bin;${currentlocation}\strawberry-perl\c\bin"
-
-
-
-cd ..
-$freerdproot = (Get-Location).Path
-$libs = "libs"
-$libspath = Join-Path $freerdproot $libs
-$env:libspath = $libspath
-if (Test-Path $libspath) {
-    Write-Host "ERROR: Libs folder exists, please remove first."
-    # cd $currentlocation
-    # exit 1
-    Remove-Item -Recurse -Force $libspath
-    New-Item -ItemType Directory -Path $libspath | Out-Null
-} else {
-    New-Item -ItemType Directory -Path $libspath | Out-Null
+    if (Test-Path("strawberry-perl")) {
+        # Remove-Item -Recurse -Force "strawberry-perl"
+        cmd.exe /c rmdir /s /q "strawberry-perl"
+    }
+    Invoke-WebRequest -Uri "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_54201_64bit/strawberry-perl-5.42.0.1-64bit-portable.zip" -OutFile "strawberry-perl.zip"
+    mkdir strawberry-perl
+    Set-Location strawberry-perl
+    tar -xf ..\strawberry-perl.zip
+    Set-Location ..
 }
 
-cd $currentlocation
+function Remove-LibsPath {
+    # Go to root of project
+    Set-Location ..
+    $freerdproot = (Get-Location).Path
+    $libs = "libs"
+    $libspath = Join-Path $freerdproot $libs
+    $env:libspath = $libspath
+    if (Test-Path $libspath) {
+        Write-Host "ERROR: Libs folder exists, please remove first."
+        # cd $currentlocation
+        # exit 1
+        cmd.exe /c rmdir /s /q $libspath
+        New-Item -ItemType Directory -Path $libspath | Out-Null
+    }
+    else {
+        New-Item -ItemType Directory -Path $libspath | Out-Null
+    }
 
+    Set-Location $currentlocation
+}
+
+Remove-TempBuildFiles
+Set-BuilderDependencies
+Remove-LibsPath
+
+# Set environment path probably not needed
 $env:PATH += ";$libspath\cJSON-install;$libspath\SDL2-install;$libspath\SDL2IMAGE-install;$libspath\uriparser-install;$libspath\zlib-install;$libspath\SDL2TTF-install;$libspath\OPENSSL-install;$libspath\libusb-install"
-
 
 & .\openssl.ps1
 & .\zlib.ps1
@@ -86,26 +99,14 @@ $env:PATH += ";$libspath\cJSON-install;$libspath\SDL2-install;$libspath\SDL2IMAG
 & .\libusb.ps1
 & .\freerdp.ps1
 
-cd $currentlocation
+Set-Location $currentlocation
 
-# cmd.exe /c rmdir /s /q openssl 
-# cmd.exe /c rmdir /s /q zlib 
-# cmd.exe /c rmdir /s /q zlib-build 
-# cmd.exe /c rmdir /s /q uriparser 
-# cmd.exe /c rmdir /s /q uriparser-build 
-# cmd.exe /c rmdir /s /q cjson 
-# cmd.exe /c rmdir /s /q cJSON-build 
-# cmd.exe /c rmdir /s /q SDL 
-# cmd.exe /c rmdir /s /q SDL-build 
-# cmd.exe /c rmdir /s /q SDL_ttf 
-# cmd.exe /c rmdir /s /q SDL_ttf-build 
-# cmd.exe /c rmdir /s /q SDL_image 
-# cmd.exe /c rmdir /s /q SDL_image-build 
-# cmd.exe /c rmdir /s /q libusb-cmake 
-# cmd.exe /c rmdir /s /q libusb-cmake-build 
+# Clean up artifacts
+# Remove-TempBuildFiles
 
 # if (Test-Path $libspath) {
-#     Remove-Item -Recurse -Force $libspath
+#     # Remove-Item -Recurse -Force $libspath
+#     cmd.exe /c rmdir /s /q $libspath
 # }
 
-cd $currentlocation
+Set-Location $currentlocation
